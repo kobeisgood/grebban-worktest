4ffa6143163f3ffa65c240ff03d2bd54375b88d9
Theodor Khademi <theo.khademi@gmail.com>
2023-03-18 10:42:46 +0100

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1> Grebban Worktest</h1>
+      <div className="puzzle-board">
+          {/* Map the tiles on the board */}
+      </div>
+
+      <button>
+        Shuffle the board
+      </button>
     </div>
   );
 }
